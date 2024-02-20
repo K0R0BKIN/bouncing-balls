f51a1701fab0e0904b1f57d14897880f10700735
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2024-02-20 09:03:25 +0000

    add shape class

--- main.js
+++ main.js
@@ -19,14 +19,21 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -40,30 +47,30 @@ class Ball {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
     }
-  
+
     if ((this.x - this.size) <= 0) {
       this.velX = -(this.velX);
     }
-  
+
     if ((this.y + this.size) >= height) {
       this.velY = -(this.velY);
     }
-  
+
     if ((this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
-  
+
     this.x += this.velX;
     this.y += this.velY;
   }
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-  
+
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
